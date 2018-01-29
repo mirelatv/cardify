5b6c3be8230e82017681955fea32cfc38d437aea
LizbethFP <lizbeth.fp@gmail.com>
2018-01-28 20:45:15 -0500

    añadiendo gitignore y corrigiendo identación index.js

--- js/index.js
+++ js/index.js
@@ -1,22 +1,17 @@
 
-(function( $ ) {
- 
+(function($) {
   $.fn.cardify = function() {
+    this.filter('img').each(function() {
+      /* debugger;*/
+      var elem = $(this);
+      var text = $(this).attr('alt');
+      console.log(text);
+      elem.wrap('<figure></figure>').parent();
+      elem.parent().append('<figcaption></figcaption>');
+      elem.next().text(text);
+      /* $('figcaption').css('display','none')*/
+    });
 
-      this.filter( "img" ).each(function() {
-         /* debugger;*/
-          var elem = $( this );
-          var text= $(this).attr('alt');
-          console.log(text);
-          elem.wrap('<figure></figure>').parent();
-          elem.parent().append('<figcaption></figcaption>');
-          elem.next().text(text);
-         /* $('figcaption').css('display','none')*/
-       
-      });
-
-      return this;
-
+    return this;
   };
-
-}( jQuery ));
+}(jQuery));
