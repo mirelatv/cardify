a5fc989ba63bf5c29df6ba3542a2989cc768626b
mirelatv <noelia.mirela@gmail.com>
2018-01-29 08:45:08 -0500

    subiendo funcion de hover sobre  img

--- js/index.js
+++ js/index.js
@@ -1,22 +1,27 @@
+(function ($) {
 
-(function( $ ) {
- 
-  $.fn.cardify = function() {
+  $.fn.cardify = function () {
 
-      this.filter( "img" ).each(function() {
-         /* debugger;*/
-          var elem = $( this );
-          var text= $(this).attr('alt');
-          console.log(text);
-          elem.wrap('<figure></figure>').parent();
-          elem.parent().append('<figcaption></figcaption>');
-          elem.next().text(text);
-         /* $('figcaption').css('display','none')*/
-       
+    this.filter("img").each(function () {
+      /* debugger;*/
+      var elem = $(this);
+      var text = $(this).attr('alt');
+      console.log(text);
+      elem.wrap('<figure></figure>').parent();
+      elem.parent().append('<figcaption></figcaption>');
+      elem.next().text(text);
+      $('figcaption').css('display', 'none');
+
+      // funcion para   mostrar el figcaption
+
+      elem.hover(function () {
+        $('figcaption').show(5000);
       });
 
-      return this;
+    });
+
+    return this;
 
   };
 
-}( jQuery ));
+}(jQuery));
