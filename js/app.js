1ea30a90151e973c0dbc85d640252b3e27751d0d
LizbethFP <lizbeth.fp@gmail.com>
2018-01-29 20:52:17 -0500

    corrigiendo identación y subiendo intento de agregar img dinámicamente

--- js/app.js
+++ js/app.js
@@ -1,7 +1,31 @@
-/* Función donde se ejecutará el plug-in creado */
+// $(document).ready(() => {
+//   /* Función para subir imágenes dinámicamente */
+//   $(function() {
+//
+//   });
+//
+//   /* Función para ejecutar plug-in*/
+//   $(function() {
+//     $('img').cardify();
+//   });
+// });
 
-$(document).ready(() => {
-$(function() {
-  $('img').cardify();
-});
+window.addEventListener('load', loadedPage => {
+  // let bodyPS = document.querySelector('.container');
+  // bodyPS.addEventListener('click',
+  // /* FUNCIÓN PARA SUBIR IMÁGENES DINÁMICAMENTE */
+  //   createImage = (filesName) => {
+  //     let imagesContainer = document.querySelector('.container');
+  //     let img = document.createElement('img');
+  //     img.setAttribute('src', 'assets/images/imagen1.jpg');
+  //     img.setAttribute('alt', imagen1);
+  //     img.classList.add('img-fluid rounded m-1') ;
+  //     imagesContainer.appendChild(img);
+  //   });
+
+  /* FUNCIÓN PARA EJECUTAR PLUG-IN */
+  $(function() {
+    $('img').cardify();
+  });
+// Llave de cierre de función general
 });
--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
-(function ($) {
-  $.fn.cardify = function () {
-    this.filter('img').each(function () {
+(function($) {
+  $.fn.cardify = function() {
+    this.filter('img').each(function() {
       /* debugger;*/
       let elem = $(this);
       let text = $(this).attr('alt');
@@ -9,44 +9,37 @@
       elem.parent().append('<figcaption></figcaption>');
       elem.next().text(text);
 
-      //Css
+      // Css
 
-      $('figure').css('display', 'inline-block');
-      $('figure').css('margin', '5%');
+      // $('figure').css('display', 'inline-block');
+      // $('figure').css('margin', '5%');
 
 
-      //Ocultando figcaption
+      // Ocultando figcaption
       $('figcaption').hide();
 
       // funcion hover para   mostrar el figcaption
 
-      elem.mouseover(function () {
+      elem.mouseover(function() {
         elem.hide(3000);
         $('figcaption').css('font-size', '30px');
         $('figcaption').show(3000);
         elem.css('margin-left', ' 40px');
-
       });
-      //funcion  mouseleve para  mostrar img y ocultar figcaption
-      elem.mouseleave(function () {
-        elem.show(3000)
+      // funcion  mouseleve para  mostrar img y ocultar figcaption
+      elem.mouseleave(function() {
+        elem.show(3000);
         $('figcaption').hide(3000);
-
       });
 
 
       // dando css al img
 
 
-
-
       // dando ccs al figcaption
-      $('figcaption').css('text-align', 'center');
-
+      // $('figcaption').css('text-align', 'center');
     });
 
     return this;
-
   };
-
 }(jQuery));
