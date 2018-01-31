489d1bd14c667b534029a911098e4e85d25bbde4
mirelatv <noelia.mirela@gmail.com>
2018-01-30 21:04:43 -0500

    borrando comentarios  de app/js y subiendo primera version de Readme

--- js/app.js
+++ js/app.js
@@ -1,16 +1,5 @@
 window.addEventListener('load', loadedPage = () => {
-  // let bodyPS = document.querySelector('.container');
-  // bodyPS.addEventListener('click',
-  // /* FUNCIÓN PARA SUBIR IMÁGENES DINÁMICAMENTE */
-  //   createImage = (filesName) => {
-  //     let imagesContainer = document.querySelector('.container');
-  //     let img = document.createElement('img');
-  //     img.setAttribute('src', 'assets/images/imagen1.jpg');
-  //     img.setAttribute('alt', imagen1);
-  //     img.classList.add('img-fluid rounded m-1') ;
-  //     imagesContainer.appendChild(img);
-  //   });
-
+  
   /* FUNCIÓN PARA EJECUTAR PLUG-IN */
   $(() => {
     $('img').cardify();
