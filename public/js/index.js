d409b5ed8e346eca99e3042c4d27d7e84a295478
LizbethFP <lizbeth.fp@gmail.com>
2018-01-31 23:54:29 -0500

    añadiendo traspilado con funcionalidad del rediseño

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,9 @@
 'use strict';
 
-(function($) {
-  $.fn.cardify = function() {
+(function ($) {
+  $.fn.cardify = function () {
     /* FUNCIÓN PARA ASIGNAR FIGURE A IMG COMO PADRE Y AGREGAR FIGCAPTION */
-    this.filter('img').each(function() {
+    this.filter('img').each(function () {
       /* debugger;*/
       var elem = $(this);
       var text = $(this).attr('alt');
@@ -17,7 +17,7 @@
         'opacity': '0',
         'text-align': 'center',
         'position': 'absolute',
-        'margin-top': '-10%',
+        'margin-top': '-14%',
         'margin-left': '10%',
         'font-size': '20px'
       });
@@ -26,7 +26,8 @@
 
       if (EventTarget == elem || elem.parent() || elem.next) {
         /* FUNCIÓN PARA ESCONDER IMAGEN Y MOSTRAR FIGCAPTION AL PASAR MOUSE*/
-        elem.mouseover(function() {
+        elem.mouseover(function () {
+          elem.parent().addClass('img-bckgrnd-color');
           elem.css('opacity', '0');
           elem.next().css({
             'opacity': '1'
@@ -34,7 +35,7 @@
         });
 
         /* FUNCIÓN PARA MOSTRAR IMAGEN Y ESCONDER FIGCAPTION AL PASAR MOUSE*/
-        elem.mouseleave(function() {
+        elem.mouseleave(function () {
           elem.css('opacity', '1');
           elem.next().css('opacity', '0');
         });
